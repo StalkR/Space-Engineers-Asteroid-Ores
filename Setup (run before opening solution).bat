e66383248cd5121ffca30fd1fcaceb2a9b17a4d3
StalkR <stalkr@stalkr.net>
2024-04-21 10:30:29 +0200

    plugin: upgrade csproj to new .NET SDK syntax

--- Setup (run before opening solution).bat
+++ Setup (run before opening solution).bat
@@ -1,18 +1,19 @@
 :: Create junctions (symlinks) to binary references to account for different installation directories on different systems.
+:: Use the dot (.) prefix so dotnet ignores the folder during build.
 
 @echo off
 
 set /p path="Please enter the folder location of your Torch.Server.exe: "
 cd %~dp0
-rmdir TorchBinaries > nul 2>&1
-mklink /J TorchBinaries "%path%"
+rmdir .TorchBinaries > nul 2>&1
+mklink /J .TorchBinaries "%path%"
 if errorlevel 1 goto Error
 echo Done!
 
 set /p path="Please enter the folder location of your SpaceEngineersDedicated.exe (e.g. Torch\DedicatedServer64):"
 cd %~dp0
-rmdir GameBinaries > nul 2>&1
-mklink /J GameBinaries "%path%"
+rmdir .GameBinaries > nul 2>&1
+mklink /J .GameBinaries "%path%"
 if errorlevel 1 goto Error
 echo Done!
 
